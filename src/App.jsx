66fd9563a1ef3a1244495318ba32956829c79fdb
Mahadi Hasan <mahadihasan8461@gmail.com>
2023-05-01 22:28:00 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+
+function App() {
+  return (
+    <>
+      <Button variant="primary">Primary</Button>{" "}
+      <Button variant="secondary">Secondary</Button>{" "}
+    </>
+  );
+}
+
+export default App;
