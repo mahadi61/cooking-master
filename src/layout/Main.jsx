977087c59b9131d5eaa0353cc4d08b0fbec131a0
Mahadi Hasan <mahadihasan8461@gmail.com>
2023-05-02 10:48:32 +0600

    banner done

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import { Container } from "react-bootstrap";
+import Banner from "../pages/home/Banner/Banner";
 import Footer from "../shared/Footer/Footer";
 import Header from "../shared/Header/Header";
 
 const Main = () => {
   return (
-    <Container>
-      <Header></Header>
+    <div>
+      <Container>
+        <Header></Header>
+      </Container>
+      <Banner></Banner>
 
       <Footer></Footer>
-    </Container>
+    </div>
   );
 };
 
--- src/pages/home/Banner/Banner.jsx
+++ src/pages/home/Banner/Banner.jsx
@@ -1,7 +1,50 @@
 import React from "react";
+import { Carousel } from "react-bootstrap";
 
 const Banner = () => {
-  return <div>This is banner</div>;
+  return (
+    <div className="">
+      <Carousel variant="dark">
+        <Carousel.Item className="">
+          <img
+            className="d-block w-100"
+            src="https://i.ibb.co/cNf62xy/Butter-Chicken.jpg"
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h5 className="text-white fs-3">First slide label</h5>
+            <p className="text-white fs-4">
+              Nulla vitae elit libero, a pharetra augue mollis interdum.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://i.ibb.co/9bc8HJ6/delicious-indian-food-tray.jpg"
+            alt="Second slide"
+          />
+          <Carousel.Caption>
+            <h5>Second slide label</h5>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://i.ibb.co/4gvmzYP/whole-chicken-grill-served-with-rice-garnish-white-plate.jpg"
+            alt="Third slide"
+          />
+          <Carousel.Caption>
+            <h5>Third slide label</h5>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </div>
+  );
 };
 
 export default Banner;
